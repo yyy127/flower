d05a3e758b0d61a0099292238cd68597ed6a25c6
yyy127 <yyy127hawks47@yahoo.co.jp>
2024-02-11 12:29:41 +0900

    settingの修正

--- myproject/settings.py
+++ myproject/settings.py
@@ -120,8 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://hackflower.azurewebsites.net',
+]
