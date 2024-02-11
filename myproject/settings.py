3e29dae8d7c8738ca0b5ed689bbdc9dd5584e3be
yyy127 <yyy127hawks47@yahoo.co.jp>
2024-02-11 19:24:17 +0900

    collectstaticの設定追加

--- myproject/settings.py
+++ myproject/settings.py
@@ -127,6 +127,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+# collectstaticコマンドが静的ファイルを集めるディレクトリ
+STATIC_ROOT = BASE_DIR / 'static'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
