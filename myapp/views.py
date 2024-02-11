65e61e212932c5659ec5a49b5140c60fd61a4b32
yyy127 <yyy127hawks47@yahoo.co.jp>
2024-02-11 11:50:11 +0900

    @csrf_protectをviewsに設定

--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_POST
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_protect
 import myapp.recipe
 import csv
 
@@ -9,6 +10,7 @@ def input(request):
     # GETリクエストの処理: 初期フォームを表示
     return render(request, 'myapp/input.html')
 
+@csrf_protect
 def response(request):
     # POSTリクエストの処理: フォームデータを変数に格納
     textbox1_value = request.POST.get('textbox1')
